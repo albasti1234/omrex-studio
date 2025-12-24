ec59425285873876ccf77ea88fbedda6b56c9574
Omar <omar.abusoud@gmail.com>
2025-12-24 04:45:02 +0400

    add: Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 ﻿import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -63,6 +64,9 @@ export default function RootLayout({
 
         {/* Footer */}
         <Footer />
+
+        {/* Vercel Speed Insights */}
+        <SpeedInsights />
       </body>
     </html>
   );
